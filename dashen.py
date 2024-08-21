9f6e4baa063e1d84c0033938905948ffd9cdf20d
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-08-21 17:21:02 +0300

    my first scraped data becom exported as csv

--- dashen.py
+++ dashen.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import pandas as pd
 
 website = "https://dashenbanksc.com/daily-exchange-rate"
 path = r'C:\Users\Mihretu\Downloads\chromedriver-win64/chromedriver.exe'
@@ -32,9 +33,39 @@ currency_code = [item for item in currency_code if item]
 currency_name = [item for item in currency_name if item]
 cash_buying = [item for item in cash_buying if item]
 cash_selling = [item for item in cash_selling if item]
-print(currency_code)
-print(currency_name)
-print(cash_buying)
-print(cash_selling)
+# print(currency_code)
+# print(currency_name)
+# print(cash_buying)
+# print(cash_selling)
 
-# driver.quit()
+
+
+whole_data = [ 
+    currency_code,
+    currency_name,
+    cash_buying,
+    cash_selling
+]
+for sublist in whole_data:
+    column_name.append(sublist[0])
+print(column_name)
+    
+columns = column_name
+    
+data = [ 
+    currency_code[1:],
+    currency_name[1:],
+    cash_buying[1:],
+    cash_selling[1:]
+]
+#Transposing the data since they are row in column and vise versa
+transposed_data = list(map(list, zip(*data)))
+print (transposed_data)
+
+df = pd.DataFrame(transposed_data, columns=column_name )
+# # Example debugging step
+# for i, row in enumerate(data):
+#     print(f"Row {i}: {row} (Length
+
+df.to_csv('dashen_scraped_data.csv', index=False)
+driver.quit()
